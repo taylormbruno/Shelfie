e3d458a37fbb342aec7f365d4f289097aba917a7
Taylor Bruno <taylorbruno895@gmail.com>
2020-02-04 20:49:56 -0500

    fixed db to display bookshelfs on main page. updated routes to include user id on home route

--- src/routes/html-routes.js
+++ src/routes/html-routes.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 // var path = require('path');
-// const db = require('../models');
+const db = require('../models');
 
 
 module.exports=function(app){
@@ -11,8 +11,36 @@ module.exports=function(app){
     });
 
     //this route needs to go to shelf page
-    app.get('/home',(req, res) =>{
-        res.render('home');
+    app.get('/home/:id',(req, res) =>{
+        db.Books.findAll({
+            subQuery: false,
+            attributes: ['id', 'book_title', 'book_id', 'book_shelf'],
+            where: {
+                UserId: req.params.id
+            }
+        }).then(function(data) {
+            
+            let cur = [];
+            let unr = [];
+            let read = [];
+            data.forEach(obj => {
+                switch (obj.book_shelf) {
+                case 'Current': cur.push(obj);
+                    break;
+                case 'Read': read.push(obj);
+                    break;
+                case 'Unread': unr.push(obj);
+                    break;
+                }
+            });
+            let hbsBooks = {
+                Current: cur,
+                Read: read,
+                Unread: unr
+            };
+            res.render('home', hbsBooks);
+        });
     });
 
 };
+
