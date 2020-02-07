131155bedae0a5db97e9a3fbecaffa67f823bf74
Leah Barnes <johnson.leahe@gmail.com>
2020-02-06 20:19:27 -0500

    adding move button and function for testing

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -118,7 +118,7 @@ module.exports = function(app) {
         db.Books.update(
             req.body, {
                 where: {
-                    id: req.body.id
+                    book_id: req.body.id
                 }
             }
         ).then(function(dbShelf) {
