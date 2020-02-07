5f6409df7eec15429416d1afd4f8e3859965b68c
Leah Barnes <johnson.leahe@gmail.com>
2020-02-06 22:07:29 -0500

    delete button showing up not deleting

--- public/js/books.js
+++ public/js/books.js
@@ -2,106 +2,127 @@
 /* eslint-disable no-undef */
 $(document).ready(function () {
 
-    // eslint-disable-next-line no-undef
-    $('#searchBtn').click(function () {
-        console.log('click');
-        var searchInput = $('#searchBar').val().trim();
-        console.log('searchInput' + searchInput);
-        // search query works and returns response
-        var queryURL = 'https://www.googleapis.com/books/v1/volumes?q=' + searchInput + '&key=AIzaSyDHWfAVWQPX7UK1qgA-EZvxpQROi2uXe_w';
-        event.preventDefault();
+  // eslint-disable-next-line no-undef
+  $('#searchBtn').click(function () {
+    console.log('click');
+    var searchInput = $('#searchBar').val().trim();
+    console.log('searchInput' + searchInput);
+    // search query works and returns response
+    var queryURL = 'https://www.googleapis.com/books/v1/volumes?q=' + searchInput + '&key=AIzaSyDHWfAVWQPX7UK1qgA-EZvxpQROi2uXe_w';
+    event.preventDefault();
 
 
-        $.ajax({
-            url: queryURL,
-            method: 'GET',
-            contentType: 'application/json'
-        }).then(function (response) {
-            $('#bookDiv').empty();
-            // logs search result/response
-            console.log(response);
+    $.ajax({
+      url: queryURL,
+      method: 'GET',
+      contentType: 'application/json'
+    }).then(function (response) {
+      $('#bookDiv').empty();
+      // logs search result/response
+      console.log(response);
 
 
-            // will have to have a loop to go through all or some (maybe 3-5?) of the response
-            for (i = 0; i < response.items.length; i++) {
-                var createImg = $('<img class=bookImg src=' + response.items[i].volumeInfo.imageLinks.smallThumbnail + '>');
-                var bookTitle = response.items[i].volumeInfo.title;
-                var bookAuthor = response.items[i].volumeInfo.authors[0];
-                var bookDesc = response.items[i].volumeInfo.description;
-                var id = response.items[i].id;
+      // will have to have a loop to go through all or some (maybe 3-5?) of the response
+      for (i = 0; i < response.items.length; i++) {
+        var createImg = $('<img class=bookImg src=' + response.items[i].volumeInfo.imageLinks.smallThumbnail + '>');
+        var bookTitle = response.items[i].volumeInfo.title;
+        var bookAuthor = response.items[i].volumeInfo.authors[0];
+        var bookDesc = response.items[i].volumeInfo.description;
+        var id = response.items[i].id;
 
-                var createSaveBtn = $('<button data-title="' + bookTitle + '"data-id="' + id + '" class="saveBtn">Add to Shelf</button>');
+        var createSaveBtn = $('<button data-title="' + bookTitle + '"data-id="' + id + '" class="saveBtn">Add to Shelf</button>');
 
-                // console.log(id);
-                console.log('book title' + bookTitle);
-                console.log('book author' + bookAuthor);
-                console.log('book desc' + bookDesc);
-                console.log('HELLOOOOOOO');
+        // console.log(id);
+        console.log('book title' + bookTitle);
+        console.log('book author' + bookAuthor);
+        console.log('book desc' + bookDesc);
+        console.log('HELLOOOOOOO');
 
-                $('#bookDiv').addClass('has-background-light');
-                $('#bookDiv').append(createImg);
-                $('#bookDiv').append('<br>');
-                // $('#bookDiv').append(createRadioBtns);
-                $('#bookDiv').append(createSaveBtn);
-                $('#bookDiv').append('<br>');
-                $('.saveBtn').addClass('button is-primary');
-                $('#bookDiv').append('<strong>Title:</strong> <span data-id="' + id + '"> ' + bookTitle + '</span><br>');
-                $('#bookDiv').append('<strong>Author:</strong> ' + bookAuthor + '<br>');
-                $('#bookDiv').append('<strong>Description:</strong> ' + bookDesc + '<br>');
-                $('#bookDiv').append('<br><br>');
-            }
-
-        });
-
+        $('#bookDiv').addClass('has-background-light');
+        $('#bookDiv').append(createImg);
+        $('#bookDiv').append('<br>');
+        // $('#bookDiv').append(createRadioBtns);
+        $('#bookDiv').append(createSaveBtn);
+        $('#bookDiv').append('<br>');
+        $('.saveBtn').addClass('button is-primary');
+        $('#bookDiv').append('<strong>Title:</strong> <span data-id="' + id + '"> ' + bookTitle + '</span><br>');
+        $('#bookDiv').append('<strong>Author:</strong> ' + bookAuthor + '<br>');
+        $('#bookDiv').append('<strong>Description:</strong> ' + bookDesc + '<br>');
+        $('#bookDiv').append('<br><br>');
+      }
 
     });
+
+
+  });
 });
 // add book to shelf
 $(document).on('click', '.saveBtn', function () {
-    event.preventDefault();
-    var dataID = $(this).attr('data-id');
-    var dataTitle = $(this).attr('data-title');
-    console.log('dataTitle ' + dataTitle);
-    console.log('dataID' + dataID);
-    let newBook = {
-        title: dataTitle,
-        isbn: dataID,
-        shelf: 'Unread'
-    };
-    $.ajax('/api/addNewBook', {
-        type: 'POST',
-        data: newBook
-    }).then(
-        function () {
-            console.log(`Created new book ${newBook}`);
-            location.reload();
-        }
-    );
+  event.preventDefault();
+  var dataID = $(this).attr('data-id');
+  var dataTitle = $(this).attr('data-title');
+  console.log('dataTitle ' + dataTitle);
+  console.log('dataID' + dataID);
+  let newBook = {
+    title: dataTitle,
+    isbn: dataID,
+    shelf: 'Unread'
+  };
+  $.ajax('/api/addNewBook', {
+    type: 'POST',
+    data: newBook
+  }).then(
+    function () {
+      console.log(`Created new book ${newBook}`);
+      location.reload();
+    }
+  );
 });
 
 $(document).on('click', '.change', function () {
-    event.preventDefault();
-    // NOT SURE AB THIS PART:
-    var readId = $(this).attr('data-id');
-    var newRead = $(this).data('newread');
+  event.preventDefault();
+  // NOT SURE AB THIS PART:
+  var readId = $(this).attr('data-id');
+  var newRead = $(this).data('newread');
 
-    console.log(readId);
-    console.log(newRead);
+  console.log(readId);
+  console.log(newRead);
 
-    var newReadState = {
-        id: readId,
-        book_shelf: newRead
-    };
-    $.ajax('/api/updateShelf', {
-        type: 'PUT',
-        data: newReadState
-    }).then(
-        function () {
-            console.log('changed unread to', newRead);
-            location.reload();
-        }
-    );
-    console.log('update', newReadState);
+  var newReadState = {
+    id: readId,
+    book_shelf: newRead
+  };
+  $.ajax('/api/updateShelf', {
+    type: 'PUT',
+    data: newReadState
+  }).then(
+    function () {
+      console.log('changed unread to', newRead);
+      location.reload();
+    }
+  );
+  console.log('update', newReadState);
 });
+
+$(document).on('click', '.delete', function () {
+  event.preventDefault();
+  console.log("Delete click");
+  var deleteId = $(this).attr('data-id');
+  console.log("deleteID: " + deleteId);
+
+  // Send the DELETE request.
+  $.ajax({
+    url: '/api/remove/' + deleteId,
+    type: "DELETE",
+    success: function (result) {
+      console.log("deleted book", deleteId);
+      // Reload the page to get the updated list
+      // location.reload();
+    }
+  })
+});
+
+    
+
 
 
--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -141,7 +141,7 @@ module.exports = function(app) {
     app.delete('/api/remove/:id', function(req, res) {
         db.Books.destroy({
             where: {
-                book_id: req.params.id
+                id: req.params.id
             }
         }).then(function(dbDelete){
             res.json(dbDelete);
